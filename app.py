080792977e4d4499ef3523261307518e47f276f8
Jan du Plessis <drjanduplessis@icloud.com>
2023-08-09 03:08:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
 import streamlit as st
 
-data = pd.read_csv('currency_rates.csv', parse_dates=['Timestamp'])
+#data = pd.read_csv('currency_rates.csv', parse_dates=['Timestamp'])
 
 st.title("Currency Tracker")
 st.markdown("Select currencies to compare:")
